80dd8085ddbc976fc8c9f55a432dee3cd1919c42
Mykyta D <nikita.derii@gmail.com>
2023-08-28 23:08:41 +0300

    Initial commit

---
+++ src/Dialog.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Dialog {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Ask the data
+        System.out.println("Enter your name, please");
+        String name = input.nextLine();
+        System.out.println("And lastname");
+        String lastname = input.nextLine();
+        System.out.println("How old are you?");
+        int age = input.nextInt();
+        System.out.println("Are you a student (true/false)?");
+        boolean student = input.nextBoolean();
+        boolean teacher = false;
+        String studentProgram = "";
+        if (!student) {
+            System.out.println("Are you a teacher then (true/false)?");
+            teacher = input.nextBoolean();
+        }
+        else {
+            System.out.println("Which program are you in?");
+            studentProgram = input.nextLine();
+        }
+
+        System.out.println("Where do you live?");
+        String city = input.nextLine();
+        System.out.println("What's your e-mail?");
+        String email = input.nextLine();
+        System.out.println("What's your phone?");
+        String phone = input.nextLine();
+        System.out.println("Do you have any pets (true/false)?");
+        boolean haspets = input.nextBoolean();
+        System.out.println("Why AUK?");
+        String whyAUK = input.nextLine();
+
+        input.nextLine();
+
+        //Show the results
+        System.out.println("Name");
+        System.out.println(name);
+        System.out.println("Lastname");
+        System.out.println(lastname);
+        System.out.println("You are " + age + " years old");
+
+        if (teacher) {
+            System.out.println("You are a teacher");
+        }
+        else {
+            System.out.println("You are a student of the " + studentProgram + " program");
+        }
+
+        System.out.println("You live in " + city);
+
+        if (phone.startsWith("+")) {
+            System.out.println("Your phone is " + phone);
+        }
+        else {
+            System.out.println("Your phone is incorrect");
+        }
+
+        if (phone.contains("@")) {
+            System.out.println("You e-mail is " + email);
+        }
+        else {
+            System.out.println("Your e-mail is incorrect");
+        }
+
+        if (haspets) {
+            System.out.println("You have pets");
+        }
+        else {
+            System.out.println("You don't have pets");
+        }
+
+        System.out.println("You have chosen AUK because " + whyAUK);
+
+        System.out.println();
+        System.out.println("Thanks!");
+    }
+}
---
+++ src/Syntax.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Syntax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Number operations
+        int i = 1;
+        double d = 1.5;
+        char c = 'c';
+        boolean b = false;
+        /*I/O operation*/System.out.println(i + d);
+
+        //String and I/O operations
+        String s1 = input.nextLine();
+        String s2 = input.nextLine();
+        System.out.println(s1 + s2);
+    }
+}
